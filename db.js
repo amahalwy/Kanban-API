40ef97e1eca6decbb9530f4e0eb4f945428b3219
Ahmed El Mahallawy <amahalwy@gmail.com>
2021-06-15 23:04:47 -0700

    Fix underline type error

--- db.js
+++ db.js
@@ -5,10 +5,10 @@ const connectDB = async () => {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
-    useFindAndModify: false
+    useFindAndModify: false,
   });
 
-  console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
+  console.log(`MongoDB Connected: ${conn.connection.host}`);
 };
 
 module.exports = connectDB;
